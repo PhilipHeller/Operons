8f0c653c510fc64cb142ba338a80140ee3f7accd
PhilipHeller <pheller@soe.ucsc.edu>
2022-01-04 07:46:14 -0800

    Add files via upload

--- OperonsReleaseFiles/EclipseProjectImage/src/operons/StatisticsBundle.java
+++ OperonsReleaseFiles/EclipseProjectImage/src/operons/StatisticsBundle.java
@@ -1,3 +1,21 @@
+/*
+ 
+Copyright 2022 Philip Heller.
+ 
+This file is part of Philip Heller's operon analysis application.
+
+The operon analysis application is free software: you can redistribute it and/or modify it under the terms of the 
+GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.
+
+The operon analysis application is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License along with the code. If not, see <https://www.gnu.org/licenses/>.
+ 
+*/
+
 package operons;
 
 import java.io.*;
